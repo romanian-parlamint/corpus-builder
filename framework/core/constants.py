3b43dcf223d74c6fd1cdb81de1d73a457423bf93
RePierre <petru.rebeja@gmail.com>
2023-05-27 17:04:55 +0300

    Replaced line feed and carriage return with empty strings

--- framework/core/constants.py
+++ framework/core/constants.py
@@ -1,6 +1,6 @@
 """Defines constants."""
 
-STR_TRANSLATIONS = str.maketrans({'\u0096': '-'})
+STR_TRANSLATIONS = str.maketrans({'\u0096': '-', '\r': '', '\n': ''})
 UD_SYN_PREFIX = 'ud-syn'
 SAMPLE_TAG = '[ParlaMint SAMPLE]'
 SAMPLE_TAG_ANA = '[ParlaMint.ana SAMPLE]'
